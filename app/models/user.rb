639a55132dbd5cade90acd6bcafac8dab1af5141
Lenilson Júnior <falecom@lenilsonjr.com>
2017-08-10 15:09:00 -0300

    tighter user devise options

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :trackable, :validatable
 end
