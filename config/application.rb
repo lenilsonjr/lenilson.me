d11aa8c31d481fd1ac69fe24d71f4e21acdc94bb
Lenilson Júnior <falecom@lenilsonjr.com>
2017-08-10 15:05:46 -0300

    application.rb configured with local timezone and generators settings

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,21 @@ module LenilsonCodes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = ActiveSupport::TimeZone.new('America/Recife')
+    config.active_record.time_zone_aware_types = [:datetime]
+    config.active_record.default_timezone = :utc # Or :utc
+
+    config.exceptions_app = self.routes
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :haml
+      g.test_framework  :test_unit, fixture: true
+      g.scaffold_stylesheet false
+      g.stylesheets     false
+      g.javascripts     false
+    end
+
   end
 end
