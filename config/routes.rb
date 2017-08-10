11c68cb96d0f807a8a4fe71e0bda0bde323e589d
Lenilson Júnior <falecom@lenilsonjr.com>
2017-08-10 15:08:12 -0300

    root path changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  namespace :frontoffice do
-    get 'home/index'
-  end
 
-  devise_for :users
+  root to: 'frontoffice/home#index'
+
+  #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
