609c7f3f3943dbef4add8d20d04c78421a4fdaa2
Lenilson DeSantis <lenilsonjunior.jr@gmail.com>
2018-03-26 08:34:41 -0300

    :wrench: now.json in shared files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
 set :repo_url, "git@github.com:lenilsonjr/lenilson.me.git"
 set :use_sudo, false
 set :keep_releases, 3
-set :linked_files, fetch(:linked_files, []).push('now.env')
+set :linked_files, fetch(:linked_files, []).push('now.env', 'now.json')
