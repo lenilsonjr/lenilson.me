cef0680309483234d031df13a765a8e929a45147
Lenilson DeSantis <lenilsonjunior.jr@gmail.com>
2018-09-16 21:36:29 -0300

    :wrench: Cap Jekyll

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,17 @@
 set :repo_url, "git@github.com:lenilsonjr/lenilson.me.git"
 set :use_sudo, false
 set :keep_releases, 3
-set :linked_files, fetch(:linked_files, []).push('now.env', 'now.json')
+set :linked_files, fetch(:linked_files, []).push('now.env', 'assets/now.json')
+set :format, :pretty
+
+namespace :deploy do
+  task :update_jekyll do
+    on roles(:app) do
+      within "#{deploy_to}/current" do
+      	execute :jekyll, "build"
+      end
+    end
+  end
+end
+
+after "deploy:symlink:release", "deploy:update_jekyll"
